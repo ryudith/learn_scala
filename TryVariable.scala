350e8c954610eed5c5873148077d52929b38674b
ryudith <ryudith_anggriawan@yahoo.co.id>
2017-08-18 00:43:25 +0700

    learn scala day 1

---
+++ TryVariable.scala
@@ -0,0 +1,13 @@
+object TryVariable {
+	def main(args: Array[String]): Unit = {
+		var intVar : Int = 10;
+		var strVar : String = "My String 1";
+		val intVal = 20;
+		val strVal = "My String 2";
+
+		println(intVar);
+		println(strVar);
+		println(intVal);
+		println(strVal);
+	}
+}
